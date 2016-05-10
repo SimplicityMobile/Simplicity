ed29825dc1330ccf5f598aa3a8e48293b0c74b54
Edward Jiang <edward@stormpath.com>
2016-05-10 16:39:40 -0700

    Initial Commit

---
+++ Simplicity/Simplicity.h
@@ -0,0 +1,19 @@
+//
+//  Simplicity.h
+//  Simplicity
+//
+//  Created by Edward Jiang on 5/10/16.
+//  Copyright © 2016 Stormpath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Simplicity.
+FOUNDATION_EXPORT double SimplicityVersionNumber;
+
+//! Project version string for Simplicity.
+FOUNDATION_EXPORT const unsigned char SimplicityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Simplicity/PublicHeader.h>
+
+
