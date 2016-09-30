c8ef24c75da637566399a350132204f147ea7936
Alex Khorin <alex@khor.in>
2016-09-30 13:12:51 +0300

    Fix opening SFSafariViewController from presented modals

--- Simplicity/Simplicity.swift
+++ Simplicity/Simplicity.swift
@@ -54,7 +54,11 @@ public final class Simplicity {
     private static func presentSafariView(_ url: URL) {
         if #available(iOS 9, *) {
             safari = SFSafariViewController(url: url)
-            UIApplication.shared.delegate?.window??.rootViewController?.present(safari!, animated: true, completion: nil)
+            var topController = UIApplication.shared.keyWindow?.rootViewController
+            while let vc = topController?.presentedViewController {
+                topController = vc
+            }
+            topController?.present(safari!, animated: true, completion: nil)
         } else {
             UIApplication.shared.openURL(url)
         }
